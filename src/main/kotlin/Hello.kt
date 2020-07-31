2da04b549406f25c6c83847bea9e71a47e116b21
Samuel French <samuelfrench@users.noreply.github.com>
2020-07-31 16:34:20 -0500

    cleanup for docker method

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -3,12 +3,13 @@ import khttp.get
 import java.io.FileInputStream
 import java.util.*
 
-fun main(args : Array<String>) {getTimeJsonString()
+fun main(args : Array<String>) {
+    println(getCurrentUnixTime())
 }
 
-fun getTimeJsonString(): String {
+fun getCurrentUnixTime(): Long {
     val props = Properties()
     props.load(FileInputStream("mockserver.properties"))
-    return get(props.getProperty("timeApiUrl")).text
+    return get(props.getProperty("timeApiUrl")).jsonObject.getLong("unixtime")
 }
 
