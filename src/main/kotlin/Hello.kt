e0805fe19c7e7067fe83fe274a1e8d57039ee7d8
Samuel French <samuelfrench@users.noreply.github.com>
2020-08-06 07:46:06 -0500

    sample app container talks to mockserver container

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -7,12 +7,18 @@ import java.util.*
 //https://www.mock-server.com/mock_server/running_mock_server.html#docker_container
 
 fun main(args : Array<String>) {
+    waitForMockserverToStartUp()
     println(getCurrentUnixTime())
+}
+
+private fun waitForMockserverToStartUp() {
+    Thread.sleep(5000);
 }
 
 fun getCurrentUnixTime(): Long {
     val props = Properties()
     props.load(FileInputStream("mockserverdemo.properties"))
+    println("debug: property file used: " + props.getProperty("timeApiUrl"))
     return get(props.getProperty("timeApiUrl")).jsonObject.getLong("unixtime")
 }
 
